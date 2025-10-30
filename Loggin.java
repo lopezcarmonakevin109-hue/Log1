02bf31653bd254a552328b0fcbe56c7231bdc21b
Kevin_Lopez_carmona <lopezcarmonakevin109@gmail.com>
2025-10-29 19:09:43 -0600

    Add files via upload

---
+++ Loggin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loggin;
+
+/**
+ *
+ * @author MAQ-10
+ */
+public class Loggin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
